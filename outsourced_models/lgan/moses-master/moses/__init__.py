0f49012d9446c9c28eb2c41de9d44a0acca618ec
samuel-renaud <59709074+samuel-renaud@users.noreply.github.com>
2021-06-12 15:25:46 -0400

    removing moses for now

--- outsourced_models/lgan/moses-master/moses/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dataset import get_dataset
-from .metrics import get_all_metrics
-from .utils import CharVocab, StringDataset
-
-
-__version__ = '0.3.1'
-__all__ = [
-    "get_dataset", "get_all_metrics", "CharVocab",
-    "StringDataset"]
